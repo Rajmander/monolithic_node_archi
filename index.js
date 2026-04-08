8459e38cc10a75b00bc445cc9c7b40ce1f90af77
rajmander <rajmandersinghmatharu@gmail.com>
2026-04-08 11:19:07 +0530

    added

--- index.js
+++ index.js
@@ -11,18 +11,27 @@ import { PORT } from "./src/config/env.js";
 //   });
 // };
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    console.log("after db connection");
+// const startServer = async () => {
+//   try {
+//     await connectDB();
+//     console.log("after db connection");
 
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error("Server failed to start:", err);
-    process.exit(1);
-  }
-};
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   } catch (err) {
+//     console.error("Server failed to start:", err);
+//     process.exit(1);
+//   }
+// };
 
-startServer();
+// startServer();
+
+// /api/index.js
+export default async function handler(req, res) {
+  console.log("THIS WILL SHOW");
+
+  await connectDB();
+
+  return app(req, res);
+}
